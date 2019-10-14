0e8648be2adbb24a4d0d601cae267310ef6c85e4
Felipe Simoes <felipe_lima_simoes@hotmail.com>
2019-10-14 16:25:51 -0300

    migration for categories and its model

--- src/database/index.js
+++ src/database/index.js
@@ -1,23 +1,23 @@
 import Sequelize from 'sequelize';
 
 import Brand from '../app/models/Brand';
+import Category from '../app/models/Category';
 
 import databaseConfig from '../config/database';
 
-const models = [Brand];
+const models = [Brand, Category];
 
 class Database {
   constructor() {
     this.init();
-    this.mongo();
+    // this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models
-      .map(model => model.init(this.connection))
-      .map(model => model.associate && model.associate(this.connection.models));
+    models.map(model => model.init(this.connection));
+    // .map(model => model.associate && model.associate(this.connection.models));
   }
 
   // mongo() {
---
+++ src/database/migrations/20191014191731-create-categories.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('categories');
+  },
+};
